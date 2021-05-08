1e40accfe9352b2100be1523fdb097e43c67f4f5
zendo <p_geondo@snu.ac.kr>
2021-05-09 00:03:48 +0900

    programmers: 짝지어 제거하기

---
+++ Programmers/level_2/pair_elimination.py
@@ -0,0 +1,19 @@
+# 짝지어 제거하기
+
+
+def solution(s):
+    answer = 0
+    stack = []
+    if (len(s)) < 2:
+        return 0
+    for i in s:
+        if len(stack) == 0:
+            stack.append(i)
+        elif stack[-1] == i:
+            stack.pop()
+        elif stack[-1] != i:
+            stack.append(i)
+    if len(stack) > 0:
+        return 0
+    else:
+        return 1
